95a712d4f95805b670aa1d19738db27e9ba172e9
Pranay Hasan <pranayhasan@gmail.com>
2016-02-26 02:42:03 +0530

    Mark Viewed Extension

---
+++ popup.js
@@ -0,0 +1,72 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    lastFocusedWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, function(tabs) {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+ getCurrentTabUrl(function(url){
+  chrome.history.getVisits({url: url}, function(results) {
+    document.getElementById('count').textContent = results.length;
+    if(results.length <= 1)
+    {
+      chrome.browserAction.setIcon({path: "red.png"});
+      document.getElementById('lastVisited').textContent = ""
+    }
+    else
+    {
+      chrome.browserAction.setIcon({path: "green.png"});
+      highest = results[0].visitTime
+      for(i=0;i<results.length;i++)
+      {
+        if(results[i].visitTime > highest)
+        {
+          highest = results[i].visitTime
+        }
+      }
+      document.getElementById('lastVisited').textContent = 'Last Viewed:'+new Date(highest)
+    }
+  });
+});
